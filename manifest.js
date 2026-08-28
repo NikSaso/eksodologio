4670ba57f5564c8c1b761ebaaa71cec3cb456e85
Sasonik <15029372+NikSaso@users.noreply.github.com>
2026-08-28 09:48:55 +0300

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+"name": "Το Εξοδολόγιό μου",
+"short_name": "Εξοδολόγιο",
+"description": "Προσωπική εφαρμογή καταγραφής εξόδων",
+"start_url": "./",
+"scope": "./",
+"display": "standalone",
+"orientation": "portrait",
+"background_color": "#111827",
+"theme_color": "#111827",
+"lang": "el",
+"categories": ["finance", "productivity"],
+"icons": [
+{
+"src": "https://cdn-icons-png.flaticon.com/512/3135/3135706.png",
+"sizes": "512x512",
+"type": "image/png",
+"purpose": "any maskable"
+}
+]
+}
